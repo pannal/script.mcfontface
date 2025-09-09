bfd7e68c6eaa9647d56802dc30b0d9868118300b
pannal <1359593+pannal@users.noreply.github.com>
2025-09-09 14:25:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Kodi Fonts
+
+
+Dynamic Kodi fonts installer that ships with the following sans-serif fonts:
+- [Hind](https://fonts.google.com/specimen/Hind?category=Sans+Serif)
+- [Inter](https://fonts.google.com/specimen/Inter?category=Sans+Serif)
+- [Lato](https://fonts.google.com/specimen/Lato?category=Sans+Serif)
+- [Montserrat](https://fonts.google.com/specimen/Montserrat?category=Sans+Serif)
+- [Open Sans](https://fonts.google.com/specimen/Open+Sans?category=Sans+Serif)
+- [PT Sans](https://fonts.google.com/specimen/PT+Sans?category=Sans+Serif)
+- [Source Sans 3](https://fonts.google.com/specimen/Source+Sans+3?category=Sans+Serif)
+
+
+When executed it installs missing fonts to `special://home/media/Fonts`.
+
+Custom fonts can be added by placing them inside `special://profile/addon_data/fonts` (folder will be created on first plugin run)
+
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/Z8Z8X6P9T)
---
+++ default.py
@@ -0,0 +1,69 @@
+# coding=utf-8
+
+import os
+import shutil
+import sys
+
+from kodi_six import xbmc, xbmcgui, xbmcvfs, xbmcaddon
+
+from lib.common import ADDON, translatePath, log, showNotification, confirmationDialog, fontFilter
+
+# check for existing fonts
+addon_folder = translatePath(os.path.join(ADDON.getAddonInfo('path')))
+target_folder = translatePath("special://home/media/Fonts")
+extra_folder = os.path.join(translatePath("special://profile/addon_data"), "fonts")
+
+permission_issue = False
+
+for folder in [target_folder, extra_folder]:
+    if not os.path.exists(folder):
+        try:
+            os.makedirs(folder)
+        except:
+            permission_issue = True
+            pass
+
+log("Installing fonts to: {}", target_folder)
+log("Looking for fonts in: {}", [extra_folder, os.path.join(addon_folder, "resources")])
+
+# add userdata/addon_data/fonts/*
+extra_fonts = list(filter(fontFilter, os.listdir(extra_folder)))
+log("Fonts in extra folder: {}", len(extra_fonts))
+
+# add supplied fonts
+our_fonts = list(filter(fontFilter, os.listdir(os.path.join(addon_folder, "resources"))))
+log("Fonts in plugin folder: {}", len(our_fonts))
+
+all_fonts = extra_fonts + our_fonts
+
+# get installed fonts
+installed_fonts = list(filter(fontFilter, os.listdir(target_folder)))
+
+install_fonts = set(all_fonts) - set(installed_fonts)
+log("Fonts left to install: {}", len(install_fonts))
+
+if not install_fonts:
+    showNotification("No fonts left to install.")
+    sys.exit(0)
+
+if not confirmationDialog("Install fonts?", "About to install {} fonts to {}".format(len(install_fonts), target_folder)):
+    sys.exit(0)
+
+
+fonts_installed = False
+log("Installing fonts: {}", install_fonts)
+for font in install_fonts:
+    base = extra_folder if os.path.exists(os.path.join(extra_folder, font)) else os.path.join(addon_folder, "resources")
+    try:
+        shutil.copyfile(os.path.join(base, font), os.path.join(target_folder, font))
+    except:
+        permission_issue = True
+        break
+
+    fonts_installed = True
+
+if permission_issue:
+    showNotification("Couldn't install fonts. Possible permission issue on data folder.")
+
+elif fonts_installed:
+    showNotification("{} fonts installed. Please restart Kodi.".format(len(install_fonts)))
